6d5b09fa57f7fd815949001a1ec099bd544ea51d
george2209 <george.dumitra@hotmail.com>
2020-11-18 13:19:10 +0100

    initialize project files

---
+++ temp_log/src/test/java/ro/gdi/stfi/temp_log/TempLogApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.gdi.stfi.temp_log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
